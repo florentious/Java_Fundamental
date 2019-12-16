b246fa12a42c88d66df771201f1d21aa78d10d6f
florentious <florentious09@gmail.com>
2019-12-16 18:26:40 +0900

    2019.12.16-v.2.0

--- src/hello/crawling/CrawlingExam.java
+++ src/hello/crawling/CrawlingExam.java
@@ -7,20 +7,23 @@ import java.util.Properties;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class CrawlingExam {
-	private static String url;
+	private String url;
 	
-	public CrawlingExam() {
-		Properties prop = new Properties();
+	
+	public void crawling() {
 		FileInputStream fis = null;
+		Properties prop = null;
 		
 		try {
-			fis = new FileInputStream("");
+			fis = new FileInputStream("C:\\dev\\workspace\\Java_Fundamental\\src\\hello\\crawling\\info.properties");
+			prop = new Properties();
 			prop.load(fis);
 			
-			this.url = prop.getProperty(url);
+			url = prop.getProperty("url2");
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -28,27 +31,42 @@ public class CrawlingExam {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			try {
+				if(fis != null) fis.close();
+			} catch (IOException e) {
+				// TODO: handle exception
+			}
 		}
 		
-		
-		
-	}
-	
-	public void crawling() {
 		Document doc = null;
 		
 		try {
 			doc = Jsoup.connect(url).get();
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		Elements elements = doc.select("query");
+		Elements elements = doc.select(".list-body.box-body.orange-body .list-desc");
+		
+		for (int i = 0; i < elements.size(); i++) {
+			Element ele = elements.get(i);
+			int index = 0;
+			String head = ele.child(index++).text();
+			String next = ele.child(index++).text();
+			String nextMore = ele.child(index++).text();
+			
+			System.out.printf("%s\t\t %s%n",head,nextMore);
+			
+			
+			
+		}
 		
 		
 	}
-	
+
 	public static void main(String[] args) {
 		new CrawlingExam().crawling();
 	}
